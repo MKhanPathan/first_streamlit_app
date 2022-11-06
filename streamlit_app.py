c12a39399715ae12c90052fecab5dd012d5297d7
Mahaboob Khan Pathan <105657133+MKhanPathan@users.noreply.github.com>
2022-11-06 18:51:23 +0530

    Update streamlit_app.py
    added breakfast menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,6 @@
 import streamlit
 streamlit.title('My first Streamlit App')
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Datmeal')
+streamlit.text('Kale, Spinach, Rocket Smoothie')
+Streamit.text('Hard-Boiled Free-Range Egg')
