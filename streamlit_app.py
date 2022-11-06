4d105127f736c3db1f77228c3609c7cf008bf38a
Mahaboob Khan Pathan <105657133+MKhanPathan@users.noreply.github.com>
2022-11-06 19:08:30 +0530

    Update streamlit_app.py
    reading fruit list from AWS S3

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,6 @@
 import streamlit
+import pandas
+
 streamlit.title('Breakfast Favorites!')
 streamlit.header('Breakfast Menu')
 streamlit.text('🍜 Omega 3 & Blueberry Datmeal')
@@ -6,3 +8,5 @@ streamlit.text('🌽 Kale, Spinach, Rocket Smoothie')
 streamlit.text('🐔 Hard-Boiled Free-Range Egg')
 streamlit.text('🍎 Apples')
 streamlit.header('🍌 🍑 Build Your Own Fruit Smoothie 🍈 🍇')
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
